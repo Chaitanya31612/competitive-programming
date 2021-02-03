f08525f79c4e946f5abed87bb048d2581363f164
Chaitanya31612 <guptachaitanya31@gmail.com>
2021-02-03 12:43:07 +0530

    02-02 arrays 4

--- Java/gfg/practice/Arrays/OneDTwoD.java
+++ Java/gfg/practice/Arrays/OneDTwoD.java
@@ -4,7 +4,7 @@
 import java.util.*;
 import java.io.*;
 
-class Abc {
+class OneDTwoD {
     public static void main(String args[]) {
         Scanner sc = new Scanner(System.in);
         int t = sc.nextInt();
